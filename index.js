3f3aacd49cd52fe40956cb5d07982ac0ed22fed5
ahmed-H20 <ahmedheshamahah8@gmail.com>
2023-09-04 01:23:53 +0300

    Make getComputerChoise function
    it make a random number between 0 to 2
    
    if the number = 0 it will return Rock
    
    if it = 1 it will return Paper and else it will return Scissors

---
+++ index.js
@@ -0,0 +1,22 @@
+// getComputerChoice
+/* return random ‘Rock’, ‘Paper’ or ‘Scissors’.*/
+function getComputerChoice{
+    let comChoice = Math.floor(Math.random() * 3);
+
+    if(comChoice == 0){
+        return "Rock";
+    }
+    else if (comChoice == 1){
+        return "Paper"
+    }else{
+        return "Scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+  // your code here!
+}
+ 
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
