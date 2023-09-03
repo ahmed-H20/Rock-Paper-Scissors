6c150e74636d1deb76229178be858245da92e341
ahmed-H20 <ahmedheshamahah8@gmail.com>
2023-09-04 02:27:30 +0300

    Make a playRound function
    it take a choise of player and random choise of computer and compare them
    
    it return a massage of result

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // getComputerChoice
-/* return random ‘Rock’, ‘Paper’ or ‘Scissors’.*/
+/* result =  random ‘Rock’, ‘Paper’ or ‘Scissors’.*/
 function getComputerChoice(){
     let comChoice = Math.floor(Math.random() * 3);
 
     if(comChoice == 0){
-        return "Rock";
+         return "Rock";
     }
     else if (comChoice == 1){
         return "Paper"
@@ -13,8 +13,46 @@ function getComputerChoice(){
     }
 }
 
+
 function playRound(playerSelection, computerSelection) {
-  // your code here!
+    let com = computerSelection.toLowerCase()
+    let player = playerSelection.toLowerCase()
+    let pointer = player + com
+    let result 
+    
+    switch(pointer){
+        case paperrock: result =  "player"
+        break;
+        case paperpaper: result =  "draw"
+        break;
+        case paperscissors: result =  "com"
+        break;
+
+        case rockrock: result =  "draw"
+        break;
+        case rockpaper: result =  "com"
+        break;
+        case rockscissors: result =  "player"
+        break;
+
+        case scissorsrock: result =  "com"
+        break;
+        case scissorspaper: result =  "player"
+        break;
+        case scissorsscissors: result =  "draw"
+        break;
+    }
+
+    if (result === "player"){
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    }
+    else if (result === "com") {
+        return `You Lose! ${playerSelection} beats ${computerSelection}`
+    }
+    else if (result === "draw"){
+        return "Draw! You and Computer are equality"
+    }
+
 }
  
 const playerSelection = "rock";
